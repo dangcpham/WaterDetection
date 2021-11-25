20bdaa66262f33741d99141c38d1169afb6b73ef
dcp237@cornell.edu <dcp237@cornell.edu>
2021-11-25 10:14:17 -0500

    change veg to aspen leaf

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ snow_path = f"{DATA_DIR}/melting_snow_1-16.9366.asc"
 sand_path = f"{DATA_DIR}/quartz_gds74.5830.asc"
 seawater_path = f"{DATA_DIR}/seawater_open_ocean_sw2.9627.asc"
 basalt_path = f"{DATA_DIR}/basalt_weathered_br93-43.7492.asc"
-veg_path = f"{DATA_DIR}/leafyspurge_spurge-a2-jun98.11306.asc"
+veg_path = f"{DATA_DIR}/aspen_leaf_dw92-2.10753.asc"
 
 surface_paths = {"snow": snow_path, 
                  "sand": sand_path,
