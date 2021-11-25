aae5a31cc032107e29735f15bf3ba7246425b7c7
dcp237@cornell.edu <dcp237@cornell.edu>
2021-11-25 10:19:49 -0500

    debug gen colors code &  gen aspen leaf colors

--- generate_colors_fake_filters.py
+++ generate_colors_fake_filters.py
@@ -15,17 +15,22 @@ N_FILTERS = int(np.floor((settings.FILTER_MAX - settings.FILTER_MIN) /
 colors = np.zeros((N_FILTERS, len(all_spectra)))
 
 for i in range(N_FILTERS):
+    # get filter response locations
     filter_pos = settings.FILTER_MIN + settings.FILTER_SIZE*(i)
-    filter_response = filter_func(filter_pos, X, step=settings.FILTER_SIZE) * all_spectra
+    # multiply spectra by filter response
+    filter_response = all_spectra * filter_func(filter_pos, X, 
+                                                step=settings.FILTER_SIZE)
+    # integrate filters (trapezoidal rule works well enough at high res.)
     colors[i] = np.trapz(filter_response, x=X, axis=1)
 
 colors = colors.transpose()
 
 # save colors data
 filter_names = [f"f{i}" for i in range(N_FILTERS)]
-pickle.dump((filter_names, colors), open("output/colors_f1.pkl", "wb"))
+pickle.dump((filter_names, colors), 
+            open(f"{settings.OUTPUT_DIR}/colors_f1.pkl", "wb"))
 
 # save filters data
 filter_info = {f"f{i}":settings.FILTER_MIN + settings.FILTER_SIZE*(i) 
-               for i in range(settings.N_FILTERS)}
-pickle.dump(filter_info, open(f"{settings.output}/filters_f1.pkl", "wb"))
+               for i in range(N_FILTERS)}
+pickle.dump(filter_info, open(f"{settings.OUTPUT_DIR}/filters_f1.pkl", "wb"))
